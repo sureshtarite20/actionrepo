5e0feff314da957dee0a84161d561df6e471e2a6
sureshtarite <suresh.tarite@publicissapient.com>
2023-06-23 17:49:53 +0530

    modified terraform files

--- Terraform-Infra/main.tf
+++ Terraform-Infra/main.tf
@@ -62,41 +62,41 @@ resource "azurerm_resource_group" "rg" {
 #   destination_address_prefix  = "*"
 # }
 
-resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
-  subnet_id                 = azurerm_subnet.subnet1.id
-  network_security_group_id = azurerm_network_security_group.nsg1.id
-}
+# resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
+#   subnet_id                 = azurerm_subnet.subnet1.id
+#   network_security_group_id = azurerm_network_security_group.nsg1.id
+# }
 
-resource "azurerm_network_interface" "nic1" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+# resource "azurerm_network_interface" "nic1" {
+#   name                = "${var.prefix}-nic"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.subnet1.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "${var.prefix}-vmt01"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s"
-  admin_username        = "adminuser"
-  admin_password        = "P@ssw0rd1234!"
-  network_interface_ids = [azurerm_network_interface.nic1.id]
+# resource "azurerm_windows_virtual_machine" "main" {
+#   name                  = "${var.prefix}-vmt01"
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   location              = azurerm_resource_group.rg.location
+#   size                  = "Standard_B1s"
+#   admin_username        = "adminuser"
+#   admin_password        = "P@ssw0rd1234!"
+#   network_interface_ids = [azurerm_network_interface.nic1.id]
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2012-R2-Datacenter"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2012-R2-Datacenter"
+#     version   = "latest"
+#   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-}
+#   os_disk {
+#     storage_account_type = "Standard_LRS"
+#     caching              = "ReadWrite"
+#   }
+# }
